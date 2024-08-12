7b142aa7058707b356753151586b0aae501e1b35
Dmitry Mezhinsky <mezhinsky.dmitry@gmail.com>
2024-08-12 12:49:12 +0300

    index page redirect

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,15 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/articles",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
